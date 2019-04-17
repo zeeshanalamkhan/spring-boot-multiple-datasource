1f4b353f052cee29ca768c6dcc873d4984a31223
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-04-17 13:38:14 +0530

    Spring boot main class

---
+++ spring-boot-multiple-datasource/src/main/java/com/zeeshan/multiple/ds/api/SpringBootMultipleDsApplication.java
@@ -0,0 +1,14 @@
+package com.zeeshan.multiple.ds.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootMultipleDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleDsApplication.class, args);
+	}
+}
